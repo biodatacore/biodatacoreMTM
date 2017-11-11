bd1f875d341598571c9ca701a8a06c905013deab
mir-cat <mir.henglin@gmail.com>
2017-11-11 15:11:58 -0500

    MAINT: Updated package helpfile from ASM to TMM

--- R/biodatacoreTMM.R
+++ R/biodatacoreTMM.R
@@ -1,10 +1,10 @@
-#' biodatacoreASM
+#' biodatacoreTMM
 #'
 #' Code to streamline omics-like analyses
 #'
 #' @docType package
 #' @name biodatacoreTMM-package
-#'
+#' @aliases biodatacoreTMM-package biodatacoreTMM
 NULL
 
 # make sure dot doesn't look like an unbound ref

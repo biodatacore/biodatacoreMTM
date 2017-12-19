52a7ca2958f63bc36ea68c2323b18e04cda73bb2
mir-cat <mir.henglin@gmail.com>
2017-12-19 11:13:50 -0500

    Data check for checking attributes of groups of variables

--- R/model_check.R
+++ R/model_check.R
@@ -70,6 +70,9 @@ base_checks <- function(data, fos) {
 model_checks <- function(data, fos, checks = NULL) {
   base_checks(data, fos)
 
+  is_fo <- rlang::is_function
+  is_vars <- ~rlang::is_character(.) || is.numeric(.)
+
 
   stopifnot(rlang::is_bare_list(checks))
   stopifnot(purrr::vec_depth(checks) == 3)
@@ -77,22 +80,84 @@ model_checks <- function(data, fos, checks = NULL) {
   purrr::walk(checks, function(check_pair) {
     stopifnot(rlang::is_bare_list(check_pair))
     stopifnot(length(check_pair) == 2)
-    stopifnot(purrr::some(check_pair, rlang::is_function))
-    stopifnot(purrr::some(check_pair, ~rlang::is_character(.) || is.numeric(.)))
+    stopifnot(purrr::some(check_pair, is_fo))
+    stopifnot(purrr::some(check_pair, is_vars))
   })
 
   check_funs <-
-    purrr::map(checks, ~purrr::keep(., rlang::is_function)) %>%
+    purrr::map(checks, ~purrr::keep(., is_fo)) %>%
     purrr::flatten()
 
   check_vars <-
-    purrr::map(checks, ~purrr::keep(., ~rlang::is_character(.) || is.numeric(.))) %>%
+    purrr::map(checks, ~purrr::keep(., is_vars)) %>%
     purrr::flatten()
 
   purrr::reduce2(check_vars, check_funs, .init = data, .f = function(dat, cv, cf) {
     dat[, cv] <- cf(dat[, cv])
     dat
   })
+
+
+}
+
+
+
+#' Apply a list of checks to a dataset
+#'
+#' @param data data_frame
+#' @param checks nested list of checks
+#'
+#' @return list
+#' @export
+#'
+data_check <- function(data, checks, individually = TRUE) {
+  stopifnot(is.data.frame(data))
+  stopifnot(rlang::is_scalar_logical(individually))
+
+  is_fo <- ~rlang::is_function(.) || rlang::is_formula(.)
+  is_vars <- ~rlang::is_character(.) || is.numeric(.)
+
+
+  stopifnot(rlang::is_bare_list(checks))
+
+  # walk-walk
+  purrr::walk(checks, function(check_pair) {
+    stopifnot(rlang::is_bare_list(check_pair))
+    stopifnot(length(check_pair) == 2)
+    stopifnot(purrr::some(check_pair, is_fo))
+    stopifnot(purrr::some(check_pair, is_vars))
+  })
+
+  check_funs <-
+    purrr::map(checks, ~purrr::keep(., is_fo)) %>%
+    purrr::flatten()
+
+  check_vars <-
+    purrr::map(checks, ~purrr::keep(., is_vars)) %>%
+    purrr::flatten()
+
+
+  purrr::map2(check_vars, check_funs, function(cv, cf) {
+    dat <-
+      data %>%
+      dplyr::select(!!cv)
+
+    if (individually) {
+      results <- purrr::map(dat, cf)
+    } else {
+      results <- list(cf(dat))
+    }
+
+    purrr::walk(results, function(result) {
+      if (!rlang::is_scalar_logical(result)) {
+        rlang::abort('Check output is not scalar logical', type = 'type_error')
+      }
+    })
+
+    results
+
+  }) %>%
+    purrr::set_names(names(checks))
 
 
 }
